885768778adbcb05bd3e0c0dd2bdffdc3982770b
tiktakz <whitehouseon@naver.com>
2021-12-23 16:05:58 +0900

    BOJ_BRONZE4_DAY1

---
+++ BaekJoon/Bronze4/1330.py
@@ -0,0 +1,8 @@
+# 1330 두 수 비교
+A, B = map(input().split())
+if A > B: 
+    print('>')
+elif A < B:
+    print('<')
+else:
+    print('==')
---
+++ BaekJoon/Bronze4/2420.py
@@ -0,0 +1,3 @@
+# 2420 사라피월드
+A, B = map(int,input().split())
+print(abs(A-B))
